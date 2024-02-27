2f3b76b624f34ff6f4373fa05ecf132cf0299648
bkylee <brian.ky.lee@outlook.com>
2024-02-26 21:52:14 -0500

    Created routes for CRUD operations for blog

--- router.js
+++ router.js
@@ -23,7 +23,38 @@ router.post("api/post/posttodb", (req, res, next) => {
   const values = [req.body.title, req.body.body, req.body.uid];
 
   pool.query(
-    `INSERT INTO posts (title, body, uid, date_created) VALUES($1,$2,$3, NOW() )`,
-    values
+    `INSERT INTO posts (title, body, date_created) VALUES($1,$2,$3, NOW() )`,
+    values,
+    (q_err, q_res) => {
+      if (q_err) return next(q_err);
+      res.json(q_res);
+    }
+  );
+});
+
+router.put("api/put/post", (req, res, next) => {
+  const values = [req.body.title, req.body.body, req.body.uid];
+
+  pool.query(
+    `UPDATE posts SET title=$1, body=$2, date_created=NOW() WHERE pid=$3`,
+    values,
+    (q_err, q_res) => {
+      res.json(q_res);
+    }
+  );
+});
+
+router.delete("api/delete/post", (req, res, next) => {
+  const postid = req.query.post_id;
+
+  pool.query(
+    `
+    DELETE FROM posts WHERE pid=$1
+    `,
+    [postid],
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+      console.log(q_err);
+    }
   );
 });
